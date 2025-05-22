d3569f309bd86cbdf3f74c3c6922e5dffb17fe79
pablo321-rgb <gonzalomarichal6@gmail.com>
2025-05-21 22:12:11 -0300

    prueba
    asfa<wf

---
+++ Edad.java
@@ -0,0 +1,58 @@
+public class Edad{
+public static void main (String[]args) { 
+    int edad = 17;
+    System.out.println("La edad es " + edad);
+}
+}
+
+
+
+public class Main {
+    public static void main (Sting)[] args) {
+        double pecioProducto = 45.99;
+        System.out.println("El precio del producto es en $" + precioProducto);
+
+    }
+}
+
+  
+public class Main {
+    public static void main(Sting[] args){  
+        boolean tieneDescuento = true;
+        System.out.println("¿El producto tiene descuento?" + tieneDescuento); 
+
+}
+
+    
+}
+
+char inicial = 'L';
+
+public class Main {
+    public static void main(String[] args) {
+        char inicial ='L'
+        System.out.println("La inicial de mi nombre es:" + L ); 
+    }
+}
+
+
+
+
+public class Main {
+    public static void main (String[]args) {
+        int puntos = 10;
+        System.out.println("Puntos iniciales:" puntos);
+        puntos = 20;
+        System.out.println("Puntos actualizados: " + puntos);
+    }
+}
+1-Una variable en Java es un espacio de memoria reservada para almacenar un valor.
+2-int,boolean.
+3-Declarar una variable, es que se puede guardar un espacio en la memoria y asignar un valor es guardar algo dentro de ese mismo espacio.
+4-Que el lenguaje Java este "tipiado" significa que las variables se aplican estrictamente. 
+5-Facilita muchas cosas como menos errores.
+6-Puede facilitar la comprención del codigo. 
+7-
+8-Puede que el copilador pueda fallar.
+9-
+10-
